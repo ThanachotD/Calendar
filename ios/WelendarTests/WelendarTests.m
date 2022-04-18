2df7c1e525893f93828578d18d97b6ab9cdd24ce
Thanachot Wongmetin <32594801+ThanachotD@users.noreply.github.com>
2022-04-18 13:57:18 +0700

    Update Welendar

--- ios/WelendarTests/WelendarTests.m
+++ ios/WelendarTests/WelendarTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SF221Tests : XCTestCase
+@interface WelendarTests : XCTestCase
 
 @end
 
-@implementation SF221Tests
+@implementation WelendarTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
